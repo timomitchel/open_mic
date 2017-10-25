30aa769327d3e90ca1cda651bed675ddd1299578
Tim Tyrrell <timomitchel23@gmail.com>
2017-10-25 09:55:51 -0600

    spacing

--- test/user_test.rb
+++ test/user_test.rb
@@ -5,6 +5,7 @@ require './lib/user'
 require "./lib/joke"
 
 class UserTest < Minitest::Test
+  
   def test_it_exists
     sal = User.new("Sal")
 
@@ -47,7 +48,7 @@ class UserTest < Minitest::Test
     ali = User.new("Ali")
     joke = Joke.new({id: 1, question: "Why did the strawberry cross the road?", answer: "Because his mother was in a jam."})
     answer = "Because his mother was in a jam."
-    
+
     sal.tell(ali, joke)
 
     assert_equal 1, ali.jokes.first.id
